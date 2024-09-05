69922bb01f289bbe2c428af0a640cd7c253b2af1
juanOllo <juan_ollo@yahoo.com>
2024-09-05 20:22:45 -0300

    muchos avances, falta ese error de las otras pags

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-const page = document.querySelector(".page");
-const cover = Array.from(document.querySelectorAll(".cover"));
+const page = Array.from(document.querySelectorAll(".page"));
+let cover = Array.from(document.querySelectorAll(".actual-page .cover"));
 const resetBtn = document.getElementById("reset");
 const nextBtn = document.getElementById("next");
 const backBtn = document.getElementById("back");
 
 cover[0].style.backgroundColor = "rgb(247, 241, 241)";
 
-for (const x of cover) {
+// for (const x of cover) {
+//     x.addEventListener("mouseover",  () => {
+//             if (x.style.backgroundColor == "rgb(247, 241, 241)") {
+//                 x.style.animation = "show-anim 0.5s forwards";
+//                 x.style.color = "red";
+//                 reColor();
+//             }
+//         })
 
-    x.addEventListener("mouseover",  () => {
-            if (x.style.backgroundColor == "rgb(247, 241, 241)") {
-                x.style.animation = "show-anim 0.5s forwards";
-                x.style.color = "red";
-                reColor();
-            }
-        })
-
-}
+// }
 
 const reColor = () => {
     if (cover[cover.length - 1].style.color !== "red"){
@@ -43,15 +42,56 @@ resetBtn.addEventListener("click", () => {
 })
 
 nextBtn.addEventListener("click", () => {
-    page.style.gridColumn = "2/3";
-    page.style.height = "4rem";
-    page.style.width = "3rem";
-    // page.style.animation = "page-old-anim 1s forwards";
+    // page.style.height = "4rem";
+    // page.style.width = "3rem";
+    // page.style.animation = "page-old-anim 2s forwards";
+
+    const actualPage = document.querySelector(".actual-page");
+    console.log(actualPage);
+
+    for (let i = 0; i < page.length - 1; i++) {
+        if (page[i] === actualPage) {
+            page[i].classList.remove("actual-page");
+            page[i+1].classList.add("actual-page");
+            i += page.length; 
+        }
+    }
+    reload();
 })
 
 backBtn.addEventListener("click", () => {
-    page.style.gridColumn = "3/4";
-    page.style.height = "40rem";
-    page.style.width = "30rem";
-    // page.style.animation = "page-old-anim 1s forwards";
+    // page.style.height = "40rem";
+    // page.style.width = "30rem";
+
+    const actualPage = document.querySelector(".actual-page");
+    console.log(actualPage);
+
+    for (let i = 1; i < page.length; i++) {
+        if (page[i] === actualPage) {
+            console.log("entroo");
+            page[i].classList.remove("actual-page");
+            page[i-1].classList.add("actual-page");
+            i += page.length; 
+        }
+    }
+    reload();
 })
+
+
+
+const reload = () => {
+    cover = Array.from(document.querySelectorAll(".actual-page .cover"));
+    cover[0].style.backgroundColor = "rgb(247, 241, 241)";
+
+    for (const x of cover) {
+        x.addEventListener("mouseover",  () => {
+                if (x.style.backgroundColor == "rgb(247, 241, 241)") {
+                    x.style.animation = "show-anim 0.5s forwards";
+                    x.style.color = "red";
+                    reColor();
+                }
+            })
+    }
+}
+
+reload();
